de6c626a6f41f1a8aecccc0c6c9c3fbb64469b23
VinayaAugusthy <vinayaaugusthy@gmail.com>
2023-10-05 16:16:20 +0530

    text widget extracted

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:foodbee/controller/restaurant_details.dart';
 import 'package:foodbee/utils/colors.dart';
 import 'package:foodbee/views/menu_details.dart';
 import 'package:foodbee/views/widgets/drawer.dart';
+import 'package:foodbee/views/widgets/text_widget.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -36,28 +37,23 @@ class HomeScreen extends StatelessWidget {
                 right: size.width * 0.01,
                 top: size.width * 0.03),
             child: SizedBox(
-              // color: Color.fromARGB(255, 222, 71, 71),
               height: size.height * 0.25,
               child: ListTile(
                 tileColor: const Color.fromARGB(255, 211, 138, 133),
                 title: Padding(
                   padding: EdgeInsets.only(top: size.width * 0.08),
-                  child: Text(
-                    restaurant.name,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: whiteColor,
-                      fontSize: 26,
-                    ),
+                  child: TextWidget(
+                    text: restaurant.name,
+                    size: 26,
+                    fontWeight: FontWeight.bold,
+                    color: whiteColor,
                   ),
                 ),
-                subtitle: Text(
-                  restaurant.address,
-                  style: TextStyle(
-                    // fontWeight: FontWeight.bold,
-                    color: whiteColor,
-                    fontSize: 20,
-                  ),
+                subtitle: TextWidget(
+                  text: restaurant.address,
+                  size: 20,
+                  fontWeight: FontWeight.normal,
+                  color: whiteColor,
                 ),
                 onTap: () {
                   Navigator.of(context).push(
@@ -76,3 +72,5 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+
